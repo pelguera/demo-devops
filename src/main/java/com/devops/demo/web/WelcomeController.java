a1aa09ea5735604f99919e1859db2c20fce01378
Paul Elguera <paul.elguera@gmail.com>
2020-02-04 00:05:58 -0500

    configuracion inicial

---
+++ src/main/java/com/devops/demo/web/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.devops.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("curso", "DevOps");
+		return "index";
+	}
+
+}
